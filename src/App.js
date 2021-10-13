21977ee4ffe76ec36da73a7a8689780ba8ac7fbc
rezasalehi <rezasalehidev@gmail.com>
2021-10-13 12:48:30 +0330

    init Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Search from "./Components/Header/Search/index";
+
+function App() {
+    return (
+        <>
+            <div className="Wrapper">
+                <Search />
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/Header/Search/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+import { FaSearch } from "react-icons/fa";
+
+const Search = () => {
+    return (
+        <div className="search">
+            <form className="search__form" action="#" onSubmit={(e) => e.preventDefault()}>
+                <input type="text" placeholder="جستجو" className="search__input" />
+
+                <button type="submit" className="search__icon">
+                    <FaSearch />
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Search;
