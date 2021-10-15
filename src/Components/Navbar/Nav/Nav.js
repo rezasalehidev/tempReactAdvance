4d7a467c09c8424ed0066c36c2c9a4c872ac7374
rezasalehi <rezasalehidev@gmail.com>
2021-10-15 22:55:15 +0330

    update menu in header

--- src/Components/Navbar/Nav/Nav.js
+++ src/Components/Navbar/Nav/Nav.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import Styles from "./style.module.css";
+import React from "react";
+import "./style.css";
 
 const Nav = () => {
     return (
-        <ul className={Styles.nav__lists}>
-            <li className={Styles.nav__list}>خانه</li>
-            <li className={Styles.nav__list}>مدرسین</li>
-            <li className={Styles.nav__list}>دوره ها</li>
-            <li className={Styles.nav__list}>درباره ما</li>
+        <ul className="nav__lists">
+            <li className="nav__list">
+                <a href="/" className="nav__link">خانه</a>
+            </li>
+            <li className="nav__list">
+                <a href="/" className="nav__link">مدرسین</a>
+            </li>
+            <li className="nav__list">
+                <a href="/" className="nav__link">دوره ها</a>
+            </li>
+            <li className="nav__list">
+                <a href="/" className="nav__link">درباره ما</a>
+            </li>
         </ul>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
