3f917156f2cd3699de4bab71e5d701e2719f6b9b
rezasalehi <rezasalehidev@gmail.com>
2021-10-17 16:33:44 +0330

    responsive navabr and finish

--- src/Components/Navbar/Nav/Nav.js
+++ src/Components/Navbar/Nav/Nav.js
@@ -1,54 +1,65 @@
 import React from "react";
 import "./style.css";
-import { FaBookMedical,FaFacebookMessenger,FaAngleDoubleLeft } from "react-icons/fa";
+import { FaBookMedical, FaFacebookMessenger, FaAngleDoubleLeft } from "react-icons/fa";
 
 const Nav = () => {
     return (
-        <ul className="nav__lists">
-            <li className="nav__list">
-                <a href="/" className="nav__link">
-                    خانه
-                </a>
-            </li>
-            <li className="nav__list">
-                <a href="/" className="nav__link">
-                    مدرسین
-                </a>
+        <div className="nav__lists_mobile">
+            <ul className="nav__lists">
+                <li className="nav__list">
+                    <a href="/" className="nav__link">
+                        خانه
+                    </a>
+                </li>
+                <li className="nav__list">
+                    <a href="/" className="nav__link">
+                        مدرسین
+                    </a>
 
-                <ul className="nav_sublists1">
-                    <li>
-                        <a href="/"><FaBookMedical/>اقای کرمی</a>
-                    </li>
-                    <li>
-                        <a href="/"><FaFacebookMessenger/>اقای مجیدی</a>
-                    </li>
-                    <li className="nav_sublist">
-                        <a href="/">سایر<FaAngleDoubleLeft/></a>
-                        <ul className="nav_sublists2">
-                            <li>
-                                <a href="/">اقای احمدی</a>
-                            </li>
-                            <li>
-                                <a href="/">اقای حسنی</a>
-                            </li>
-                            <li>
-                                <a href="/">خانم محسنی</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li className="nav__list">
-                <a href="/" className="nav__link">
-                    دوره ها
-                </a>
-            </li>
-            <li className="nav__list">
-                <a href="/" className="nav__link">
-                    درباره ما
-                </a>
-            </li>
-        </ul>
+                    <ul className="nav_sublists1">
+                        <li>
+                            <a href="/">
+                                <FaBookMedical />
+                                اقای کرمی
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <FaFacebookMessenger />
+                                اقای مجیدی
+                            </a>
+                        </li>
+                        <li className="nav_sublist">
+                            <a href="/">
+                                سایر
+                                <FaAngleDoubleLeft />
+                            </a>
+                            <ul className="nav_sublists2">
+                                <li>
+                                    <a href="/">اقای احمدی</a>
+                                </li>
+                                <li>
+                                    <a href="/">اقای حسنی</a>
+                                </li>
+                                <li>
+                                    <a href="/">خانم محسنی</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li className="nav__list">
+                    <a href="/" className="nav__link">
+                        دوره ها
+                    </a>
+                </li>
+                <li className="nav__list">
+                    <a href="/" className="nav__link">
+                        درباره ما
+                    </a>
+                </li>
+            </ul>
+        </div>
     );
 };
 
